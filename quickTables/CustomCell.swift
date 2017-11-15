949b8a90b268fea768101e635a0a3435da2e4a2b
feodor <feodor.mak@gmail.com>
2017-11-15 10:28:57 +0800

    jj

--- quickTables/CustomCell.swift
+++ quickTables/CustomCell.swift
@@ -10,8 +10,26 @@ import UIKit
 
 class CustomCell: UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     
-    @IBOutlet weak var tableCollectionView: UICollectionView!
+    @IBOutlet private weak var tableCollectionView: UICollectionView!
+    @IBOutlet private weak var collectionViewHeight: NSLayoutConstraint!
     
+    @IBOutlet private weak var leadingConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var trailingConstraint: NSLayoutConstraint!
+    
+    //constants
+    private let leftSpacing: CGFloat = 1.0
+    
+    var data: [[String]]? {
+        didSet {
+            if data != nil {
+                self.tableSizing()
+                self.collectionViewHeight.constant = 1.0 + CGFloat(data!.count) * (30.0 + 1.0)
+            }
+            
+        }
+    }
+    private var cellMaxWidths = [CGFloat]()
+    private var actualWidths = [CGFloat]()
     
     static var identifier: String {
         return String(describing: self)
@@ -26,17 +44,27 @@ class CustomCell: UITableViewCell, UICollectionViewDataSource, UICollectionViewD
         tableCollectionView.register(TableCell.nib, forCellWithReuseIdentifier: TableCell.identifier)
         tableCollectionView.dataSource = self
         tableCollectionView.delegate = self
-        tableCollectionView.backgroundColor = UIColor.red
+        tableCollectionView.backgroundColor = UIColor.black
+        tableCollectionView.isUserInteractionEnabled = false
+        
+        print(self.frame.width)
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        self.cellMaxWidths.removeAll()
+        self.leadingConstraint.constant = 2.0
+        self.trailingConstraint.constant = 2.0
+        
+        
     }
+    
     
     
     //this is ROWS
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
+        if data != nil { return self.data!.count }
+        else { return 0 }
     }
     
     //this is COLUMNS
@@ -46,12 +74,44 @@ class CustomCell: UITableViewCell, UICollectionViewDataSource, UICollectionViewD
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TableCell.identifier, for: indexPath) as! TableCell
+        if data != nil {
+            cell.cellLabel.text = data![indexPath.section][indexPath.row]
+        }
+        
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 50, height: 50)
+        return CGSize(width: cellMaxWidths[indexPath.row], height: 30)
     }
     
     
+    
+    private func tableSizing() {
+        guard data != nil else {
+            return
+        }
+        for row  in data! {
+            for (index, cell) in row.enumerated() {
+                let width = self.maximumLabelWidth(text: cell)
+                if cellMaxWidths.count < data![0].count { cellMaxWidths.append(width) }
+                else if width > cellMaxWidths[index]{ cellMaxWidths[index] = width }
+            }
+        }
+
+        print(self.frame.width, tableCollectionView.frame.width, cellMaxWidths.reduce(0, +))
+        if cellMaxWidths.reduce(0, +) <= tableCollectionView.frame.width - (CGFloat(cellMaxWidths.count + 1) * leftSpacing) {
+            let totalSpacing = CGFloat(cellMaxWidths.count+1) * leftSpacing
+            //print(tableCollectionView.frame.width, cellMaxWidths.reduce(0, +), newConstraintConstant)
+            self.leadingConstraint.constant = (self.tableCollectionView.frame.width -  totalSpacing - cellMaxWidths.reduce(0, +)) / 2
+            self.trailingConstraint.constant = (self.tableCollectionView.frame.width -  totalSpacing - cellMaxWidths.reduce(0, +)) / 2
+        }
+        
+    }
+    
+    private func maximumLabelWidth(text: String)-> CGFloat {
+        let label = UILabel()
+        label.text = text
+        return label.textRect(forBounds: CGRect(x: 0, y:0, width:self.frame.width, height: 50), limitedToNumberOfLines: 1).width
+    }
 }
--- quickTables/ViewController.swift
+++ quickTables/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 import WebKit
 
 class ViewController: UITableViewController {
-
-    let text = "<table><tr><td>11</td><td>12</td><td>13</td></tr><tr><td>21</td><td>22</td><td>23</td></tr></table>"
+    
+    //let text = "<table><tr><td>11</td><td>12</td><td>13</td></tr><tr><td>21</td><td>22</td><td>23</td></tr></table>"
+    
+    let text = "<table><tr><td><b>Airport</b></td><td><b>Airport Code (ICAO / IATA)</b></td></tr><tr><td>Bangalore</td><td>VOBG / BLR</td></tr><tr><td>Bangkok</td><td>VTBS / BKK</td></tr><tr><td>Broome</td><td>YBRM / BME</td></tr><tr><td><b>Chennai</b></td><td><b>VOMM / MAA</b></td></tr><tr><td>Chiang Mai</td><td>VTCC / CNX</td></tr><tr><td>Colombo</td><td>VCBI / CMB</td></tr><tr><td>Curtin</td><td>YCIN / DCN</td></tr><tr><td><b>Darwin</b></td><td><b>YPDN / DRW</b></td></tr><tr><td>Denpasar (Bali)</td><td>WADD / DPS</td></tr><tr><td><b>Hyderabad</b></td><td><b>VOHS / HYD</b></td></tr><tr><td><b>Kochi</b></td><td><b>VOCI / COK</b></td></tr><tr><td><b>Kuala Lumpur</b></td><td><b>WMKK / KUL</b></td></tr><tr><td>Kupang</td><td>WATT / KOE</td></tr><tr><td>Langkawi</td><td>WMKL / LGK</td></tr><tr><td>Lombok</td><td>WADL / LOP</td></tr><tr><td>Male</td><td>VRMM / MLE</td></tr><tr><td><b>Medan</b></td><td><b>WIMM / KNO</b></td></tr><tr><td>Penang</td><td>WMKP / PEN</td></tr><tr><td><b>Phuket</b></td><td><b>VTSP / HKT</b></td></tr><tr><td>Port Hedland</td><td>YPPD / PHE</td></tr><tr><td>Singapore</td><td>WSSS / SIN</td></tr><tr><td>Surabaya</td><td>WARR / SUB</td></tr><tr><td>Tindal</td><td>YPTN / KTR</td></tr><tr><td><b>Trivandrum</b></td><td><b>VOTV / TRV</b></td></tr><tr><td><b>Ujung Pandang (Makassar)</b></td><td><b>WAAA / UPG</b></td></tr><tr><td>Vishakhapatnam</td><td>VOVZ / VTZ</td></tr><tr><td>Yangon</td><td>VYYY / RGN</td></tr></table>"
     var tableData = [[String]]()
     
     override func viewDidLoad() {
@@ -33,30 +35,61 @@ class ViewController: UITableViewController {
             }
         }
         catch {}
-     
-        self.tableView.tableFooterView = UIView()
-        self.view.backgroundColor = .gray
-        print(tableData)
         
-
+        self.tableView.tableFooterView = UIView()
+        self.tableView.estimatedRowHeight = 100
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.layoutMargins = UIEdgeInsets.zero
+        self.view.backgroundColor = .gray
+        
+        print(self.view.frame.width)
+        //print(tableData)
+        
+        
     }
-
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
+        return UITableViewAutomaticDimension
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.identifier, for: indexPath)
-        return cell
-        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.identifier, for: indexPath) as? CustomCell{
+            cell.data = self.tableData
+            print(cell.frame.width)
+            return cell
+        }
+        else { return UITableViewCell() }
     }
     
-
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: false)
+    }
+    
+    
 }
 
+extension UILabel {
+    func setHTMLFromString(htmlText: String) {
+        let modifiedFont = NSString(format:"<span style=\"font-family: '-apple-system', 'HelveticaNeue'; font-size: \(self.font!.pointSize)\">%@</span>" as NSString, htmlText) as String
+        
+        
+        //process collection values
+        let attrStr = try! NSMutableAttributedString(
+            data: modifiedFont.data(using: .unicode, allowLossyConversion: true)!,
+            options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue],
+            documentAttributes: nil)
+        
+        if attrStr.string.hasSuffix("\n") {
+            attrStr.mutableString.deleteCharacters(in: attrStr.mutableString.rangeOfComposedCharacterSequence(at: attrStr.mutableString.length - 1))
+            //attrStr.mutableString.setString(attrStr.string.substring(to: attrStr.string.index(attrStr.string.endIndex, offsetBy: -1)))
+        }
+        
+        self.attributedText = attrStr
+        
+    }
+}
