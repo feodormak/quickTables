9209b7dd6e40ce1b1b314defd0f16be90382788f
feodor <feodor.mak@gmail.com>
2017-11-17 20:34:29 +0800

    update

---
+++ quickTables/QuickTable.swift
@@ -0,0 +1,154 @@
+//
+//  File.swift
+//  quickTables
+//
+//  Created by feodor on 14/11/17.
+//  Copyright © 2017 feodor. All rights reserved.
+//
+
+import UIKit
+
+enum QuickTableConstants {
+    static let leftSpacing: CGFloat = 1.0
+    static let defaultSpacing: CGFloat = 2.0
+    static let tabellCellLabelSpacing:CGFloat = 4.0
+    static let minRowHeight:CGFloat = 30
+}
+
+class QuickTable: UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    @IBOutlet private weak var tableCollectionView: UICollectionView!
+    @IBOutlet private weak var tableCollectionViewHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet private weak var leadingConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var trailingConstraint: NSLayoutConstraint!
+    
+    //constants
+    private var data: [[NSAttributedString]]? { didSet { if data != nil { tableCollectionView.reloadData() } } }
+    private var parentWidth: CGFloat? { didSet{ if parentWidth != nil { self.tableSizing(width: parentWidth!) } } }
+    
+    private var cellMaxWidths = [CGFloat]()
+    private var rowHeights = [CGFloat]()
+    private var tableHeight:CGFloat = 1
+    
+    static var identifier: String { return String(describing: self) }
+    static var nib:UINib { return UINib(nibName: identifier, bundle: nil) }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        tableCollectionView.register(TableCell.nib, forCellWithReuseIdentifier: TableCell.identifier)
+        tableCollectionView.dataSource = self
+        tableCollectionView.delegate = self
+        tableCollectionView.backgroundColor = UIColor.black
+        tableCollectionView.isUserInteractionEnabled = false
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.cellMaxWidths.removeAll()
+        self.rowHeights.removeAll()
+        self.tableHeight = 1
+        self.data = nil
+        self.parentWidth = nil
+        
+        self.leadingConstraint.constant = QuickTableConstants.defaultSpacing
+        self.trailingConstraint.constant = QuickTableConstants.defaultSpacing
+    }
+    
+    //this is ROWS
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        if data != nil { return self.data!.count }
+        else { return 0 }
+    }
+    
+    //this is COLUMNS
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if self.data != nil {
+            if self.data!.first != nil { return self.data!.first!.count }
+            else { return 0 }
+        }
+        else { return 0 }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TableCell.identifier, for: indexPath) as? TableCell {
+            if self.data != nil {
+                cell.cellLabel.attributedText = self.data![indexPath.section][indexPath.row]
+                return cell
+            }
+            return UICollectionViewCell()
+        }
+        else { return UICollectionViewCell() }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: cellMaxWidths[indexPath.row], height: rowHeights[indexPath.section])
+    }
+    
+    func setupTable(width: CGFloat, data: [[NSAttributedString]]) {
+        self.data = data
+        self.parentWidth = width
+    }
+    
+    private func tableSizing(width: CGFloat) {
+        self.getCellMaxWidths()
+        
+        guard self.cellMaxWidths.isEmpty != true else { return }
+        
+        if self.cellMaxWidths.reduce(0, +) <= width - (2 * QuickTableConstants.defaultSpacing) - (CGFloat(self.cellMaxWidths.count + 1) * QuickTableConstants.leftSpacing) {
+            let totalSpacing = CGFloat(self.cellMaxWidths.count+1) * QuickTableConstants.leftSpacing
+            let newConstraintConstant = (width -  totalSpacing - self.cellMaxWidths.reduce(0, +)) / 2
+            self.leadingConstraint.constant = newConstraintConstant
+            self.trailingConstraint.constant = newConstraintConstant
+        }
+        else{
+            let totalRequiredWidth = self.cellMaxWidths.reduce(0, +)
+            let useableWidthForCells = width - (2 * QuickTableConstants.defaultSpacing) - CGFloat(self.cellMaxWidths.count + 1) * QuickTableConstants.leftSpacing
+            var useableWidthLeft = useableWidthForCells
+            for (index, cell) in self.cellMaxWidths.enumerated() {
+                self.cellMaxWidths[index] = (index + 1) == self.cellMaxWidths.count ? useableWidthLeft : round((cell / totalRequiredWidth * useableWidthForCells))
+                useableWidthLeft -= self.cellMaxWidths[index]
+            }
+        }
+        self.getRowHeights()
+        self.tableCollectionViewHeightConstraint.constant = self.tableHeight
+    }
+    
+    private func getCellMaxWidths() {
+        guard self.data != nil else { return }
+        for row in self.data! {
+            for (index, cell) in row.enumerated() {
+                let width = self.maximumLabelWidth(text: cell)
+                if self.cellMaxWidths.count < self.data![0].count { self.cellMaxWidths.append(width) }
+                else if width > self.cellMaxWidths[index]{ self.cellMaxWidths[index] = width }
+            }
+        }
+    }
+    
+    private func maximumLabelWidth(text: NSAttributedString)-> CGFloat {
+        let label = UILabel()
+        label.attributedText = text
+        return label.textRect(forBounds: CGRect(x: 0, y:0, width:UIScreen.main.bounds.width, height: 50), limitedToNumberOfLines: 1).width + 2*QuickTableConstants.tabellCellLabelSpacing
+    }
+    
+    private func getRowHeights() {
+        guard data != nil && rowHeights.isEmpty else { return }
+        for row in data! {
+            var rowHeightArray = [CGFloat]()
+            for (index,cell) in row.enumerated() {
+                if let nibCell = TableCell.fromNib() {
+                    nibCell.cellLabel.preferredMaxLayoutWidth = self.cellMaxWidths[index] - 2 * QuickTableConstants.tabellCellLabelSpacing * CGFloat(cellMaxWidths.count)
+                    nibCell.cellLabelWidth.constant = nibCell.cellLabel.preferredMaxLayoutWidth + 2*QuickTableConstants.tabellCellLabelSpacing
+                    nibCell.cellLabel.attributedText = cell
+                    
+                    let cellHeight = nibCell.contentView.systemLayoutSizeFitting(UILayoutFittingCompressedSize).height > QuickTableConstants.minRowHeight ? nibCell.contentView.systemLayoutSizeFitting(UILayoutFittingCompressedSize).height : QuickTableConstants.minRowHeight
+                    rowHeightArray.append(cellHeight)
+                }
+            }
+            if let maximumHeightInRow = rowHeightArray.max() {
+                self.tableHeight += maximumHeightInRow + 1.0
+                self.rowHeights.append(maximumHeightInRow)
+            }
+        }
+    }
+}
--- quickTables/ViewController.swift
+++ quickTables/ViewController.swift
@@ -13,29 +13,64 @@ class ViewController: UITableViewController {
     
     let text = "<table><tr><td>11</td><td>12</td><td>13</td></tr><tr><td>21</td><td>22</td><td>23</td></tr></table>"
     
-    let text2 = "<table><tr><td><b>Airport</b></td><td><b>Airport Code (ICAO / IATA)</b></td></tr><tr><td>Bangalore</td><td>VOBG / BLR</td></tr><tr><td>Bangkok</td><td>VTBS / BKK</td></tr><tr><td>Broome</td><td>YBRM / BME</td></tr><tr><td><b>Chennai</b></td><td><b>VOMM / MAA</b></td></tr><tr><td>Chiang Mai</td><td>VTCC / CNX</td></tr><tr><td>Colombo</td><td>VCBI / CMB</td></tr><tr><td>Curtin</td><td>YCIN / DCN</td></tr><tr><td><b>Darwin</b></td><td><b>YPDN / DRW</b></td></tr><tr><td>Denpasar (Bali)</td><td>WADD / DPS</td></tr><tr><td><b>Hyderabad</b></td><td><b>VOHS / HYD</b></td></tr><tr><td><b>Kochi</b></td><td><b>VOCI / COK</b></td></tr><tr><td><b>Kuala Lumpur</b></td><td><b>WMKK / KUL</b></td></tr><tr><td>Kupang</td><td>WATT / KOE</td></tr><tr><td>Langkawi</td><td>WMKL / LGK</td></tr><tr><td>Lombok</td><td>WADL / LOP</td></tr><tr><td>Male</td><td>VRMM / MLE</td></tr><tr><td><b>Medan</b></td><td><b>WIMM / KNO</b></td></tr><tr><td>Penang</td><td>WMKP / PEN</td></tr><tr><td><b>Phuket</b></td><td><b>VTSP / HKT</b></td></tr><tr><td>Port Hedland</td><td>YPPD / PHE</td></tr><tr><td>Singapore</td><td>WSSS / SIN</td></tr><tr><td>Surabaya</td><td>WARR / SUB</td></tr><tr><td>Tindal</td><td>YPTN / KTR</td></tr><tr><td><b>Trivandrum</b></td><td><b>VOTV / TRV</b></td></tr><tr><td><b>Ujung Pandang (Makassar)</b></td><td><b>WAAA / UPG</b></td></tr><tr><td>Vishakhapatnam</td><td>VOVZ / VTZ</td></tr><tr><td>Yangon</td><td>VYYY / RGN</td></tr></table>"
+    let text2 = "<style>table, th, td { border: 1px solid black;}</style><table><tr><td style=\"background-color:#bdc3c7\"><p style=\"color:white\"><b>Airport</b></td><td style=\"background-color:#bdc3c7\"><p style=\"color:white\"><b>Airport Code (ICAO / IATA)</b></td></tr><tr><td>Bangalore</td><td>VOBG / BLR</td></tr><tr><td>Bangkok</td><td>VTBS / BKK</td></tr><tr><td>Broome</td><td>YBRM / BME</td></tr><tr><td><b>Chennai</b></td><td><b>VOMM / MAA</b></td></tr><tr><td>Chiang Mai</td><td>VTCC / CNX</td></tr><tr><td>Colombo</td><td>VCBI / CMB</td></tr><tr><td>Curtin</td><td>YCIN / DCN</td></tr><tr><td><b>Darwin</b></td><td><b>YPDN / DRW</b></td></tr><tr><td>Denpasar (Bali)</td><td>WADD / DPS</td></tr><tr><td><b>Hyderabad</b></td><td><b>VOHS / HYD</b></td></tr><tr><td><b>Kochi</b></td><td><b>VOCI / COK</b></td></tr><tr><td><b>Kuala Lumpur</b></td><td><b>WMKK / KUL</b></td></tr><tr><td>Kupang</td><td>WATT / KOE</td></tr><tr><td>Langkawi</td><td>WMKL / LGK</td></tr><tr><td>Lombok</td><td>WADL / LOP</td></tr><tr><td>Male</td><td>VRMM / MLE</td></tr><tr><td><b>Medan</b></td><td><b>WIMM / KNO</b></td></tr><tr><td>Penang</td><td>WMKP / PEN</td></tr><tr><td><b>Phuket</b></td><td><b>VTSP / HKT</b></td></tr><tr><td>Port Hedland</td><td>YPPD / PHE</td></tr><tr><td>Singapore</td><td>WSSS / SIN</td></tr><tr><td>Surabaya</td><td>WARR / SUB</td></tr><tr><td>Tindal</td><td>YPTN / KTR</td></tr><tr><td><b>Trivandrum</b></td><td><b>VOTV / TRV</b></td></tr><tr><td><b>Ujung Pandang (Makassar)</b></td><td><b>WAAA / UPG</b></td></tr><tr><td>Vishakhapatnam</td><td>VOVZ / VTZ</td></tr><tr><td>Yangon</td><td>VYYY / RGN</td></tr></table>"
     
     //let text3 = "<table><tr><td>1111111111111</td><td>12</td><td>13</td></tr><tr><td>21</td><td>22</td><td>23133213231231</td></tr></table>"
-    let text3 = "<table><tr><td><b>AIRSPACE RNP</b></td><td><b>Requirement</b></td></tr><tr><td>Australia</td><td>10</td></tr><tr><td>Australia Eastern Oceanic Area</td><td>4</td></tr><tr><td>Bay of Bengal</td><td>10</td></tr><tr><td>China (only within Sanya AOR)</td><td>10</td></tr><tr><td>Hong Kong</td><td>4 or 10*</td></tr><tr><td>India</td><td>10**</td></tr><tr><td>Indonesia</td><td>10</td></tr><tr><td>Malaysia</td><td>10</td></tr><tr><td>South China Sea</td><td>10</td></tr><tr><td>Thailand &amp; Vietnam</td><td>10</td></tr></tbody></table>"
+    //let text3 = "<table><tr><td><b>AIRSPACE RNP</b></td><td><b>Requirement</b></td></tr><tr><td>Australia</td><td>10</td></tr><tr><td>Australia Eastern Oceanic Area</td><td>4</td></tr><tr><td>Bay of Bengal</td><td>10</td></tr><tr><td>China (only within Sanya AOR)</td><td>10</td></tr><tr><td>Hong Kong</td><td>4 or 10*</td></tr><tr><td>India</td><td>10**</td></tr><tr><td>Indonesia</td><td>10</td></tr><tr><td>Malaysia</td><td>10</td></tr><tr><td>South China Sea</td><td>10</td></tr><tr><td>Thailand &amp; Vietnam</td><td>10</td></tr></tbody></table>"
     
-    let text4 = "<table><tr><td>ATS DATALINK PROVIDER</td><td>LOGON ADDRESS</td><td>AREA OF OPERATION</td><td>DATALINK POSITION REPORT</td><td>DATALINK SERVICE AVAILABLE</td></tr><tr><td>SINGAPORE</td><td>WSJC</td><td>SIN FIR over South China Sea</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>YANGON</td><td>VYYF</td><td>Within YANGON FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>COLOMBO</td><td>VCCF</td><td>Within COLOMBO FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>CHENNAI</td><td>VOMF</td><td>Within CHENNAI FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>KOLKATA</td><td>VECF</td><td>Within KOLKATA FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>FUKUOKA</td><td>RJJJ</td><td>Within FUKUOKA FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>HONG KONG</td><td>VHHH</td><td>Within HONG KONG FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>UJUNG PANDANG</td><td>WAAF</td><td>Within UJUNG PANDANG FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>HO CHI MINH</td><td>WAAF</td><td>Within HO CHI MINH FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr></table>"
+    //let text4 = "<table><tr><td>ATS DATALINK PROVIDER</td><td>LOGON ADDRESS</td><td>AREA OF OPERATION</td><td>DATALINK POSITION REPORT</td><td>DATALINK SERVICE AVAILABLE</td></tr><tr><td>SINGAPORE</td><td>WSJC</td><td>SIN FIR over South China Sea</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>YANGON</td><td>VYYF</td><td>Within YANGON FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>COLOMBO</td><td>VCCF</td><td>Within COLOMBO FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>CHENNAI</td><td>VOMF</td><td>Within CHENNAI FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>KOLKATA</td><td>VECF</td><td>Within KOLKATA FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>FUKUOKA</td><td>RJJJ</td><td>Within FUKUOKA FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>HONG KONG</td><td>VHHH</td><td>Within HONG KONG FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>UJUNG PANDANG</td><td>WAAF</td><td>Within UJUNG PANDANG FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr><tr><td>HO CHI MINH</td><td>WAAF</td><td>Within HO CHI MINH FIR</td><td>At Compulsory* Reporting Points</td><td>CPDLC ADS</td></tr></table>"
+    
+    /*
+    let text = [["11231231", "dshsdfg" ,"2" ,"fhdshdh"],
+                ["ncvbjd","44ghj","wertwetw43t3","gsdgg"],
+                ["rtu46 u6rhtr u","cdyk gst se ","sgdf","8 ds fd f"],
+                ["i6r7 es5yr","s ftrueb", "5se64","453e"],
+                [" a5y35y4","a yaye"," d8ut yert ","7 5ysdr"],
+                ["s 45y4 s4","s e5yse"," sey s45y","s 53ys"]]
+    let text2 = [["342","32452","fgdhfu"],
+                 ["aaaaaaaaaaaaaaa","dddddddd","d"],
+                 ["a","b","c"],
+                 ["asdf","adfa","adfa"],
+                 ["atvegav","avtawtv","a4ta34tva"],
+                 ["z34v3","z z 4t34 3","vzv grd yey"],
+                 ["fgfdhd","zvgsdvgz","sdfgsdfvgsd"],
+                 ["sgvfdsvgd","sdgsvdvd","dsgsvf"],
+                 ["sfsddfs","svdgf","sdfsfdvs"],
+                 ["sdfgsd d","sdfgs d","svfgvdfv"],
+                 ["vsdfvgsd","svfsddv","sdvfgvdfv"],
+                 ["sdgfdgs","sdvfdvgdv","svfsdgvd"],
+                 ["sdgds","dgfsd","sdgdfsg"],
+                 ["g","sgfdsd","sdfgsd"],
+                 ["g","fgsg","dsfgfds"],
+                 ["sdg","segsd  sgfs","664356345"]]
+       */
     
     var tableData = [[[NSAttributedString]]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        self.tableView.register(CustomCell.nib, forCellReuseIdentifier: CustomCell.identifier)
+        self.tableView.register(QuickTable.nib, forCellReuseIdentifier: QuickTable.identifier)
         
-        for textToProcess in [text, text2, text3, text4]{
+        for textToProcess in [text, text2]{
             var subArray = [[NSAttributedString]]()
+            /*
+            for row in textToProcess {
+                var subsubArrary = [NSMutableAttributedString]()
+                for cell in row { subsubArrary.append(NSMutableAttributedString.init(string: cell)) }
+                subArray.append(subsubArrary)
+            }
+            */
+            
             do {
                 let doc = try HTML(html: textToProcess, encoding: .utf8)
+                if let style = doc.at_css("style") { print(style.at_css("border")) }
                 if let body = doc.body {
+                    
                     for row in body.css("tr") {
                         var subsubArray = [NSAttributedString]()
                         for cell in row.css("td") {
+                            
                             if let cellData = cell.innerHTML { subsubArray.append(cellData.attributedStringFromHTMLString()) }
                         }
                         subArray.append(subsubArray)
@@ -54,7 +89,7 @@ class ViewController: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 0
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -62,9 +97,8 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.identifier, for: indexPath) as? CustomCell{
-            cell.data = self.tableData[3]
-            cell.parentWidth = tableView.frame.width
+        if let cell = tableView.dequeueReusableCell(withIdentifier: QuickTable.identifier, for: indexPath) as? QuickTable{
+            cell.setupTable(width: tableView.frame.width, data: self.tableData[indexPath.row%2])
             return cell
         }
         else { return UITableViewCell() }
